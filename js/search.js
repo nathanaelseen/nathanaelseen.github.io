8a2d72796c7d9c8f622dbbebb3d52bd1b8949c6d
Nathanael Seen <e0345677@u.nus.edu>
2020-05-18 21:59:29 +0800

    Complete searchfield suggestions

--- js/search.js
+++ js/search.js
@@ -1,61 +1,201 @@
-(function() {
-  function displaySearchResults(results, store) {
+function displaySearchResults(results, store) {
     var searchResults = document.getElementById('search-results');
 
     if (results.length) { // Are there any results?
-      var appendString = '';
+        var appendString = '';
 
-      for (var i = 0; i < results.length; i++) {  // Iterate over the results
-        var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
-      }
+        for (var i = 0; i < results.length; i++) {  // Iterate over the results
+            var item = store[results[i].ref];
+            appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+            appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+        }
 
-      searchResults.innerHTML = appendString;
+        searchResults.innerHTML = appendString;
     } else {
-      searchResults.innerHTML = '<li>No results found</li>';
+        searchResults.innerHTML = '<li>No results found</li>';
     }
-  }
+}
 
-  function getQueryVariable(variable) {
+function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split('&');
 
     for (var i = 0; i < vars.length; i++) {
-      var pair = vars[i].split('=');
+        var pair = vars[i].split('=');
 
-      if (pair[0] === variable) {
-        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-      }
+        if (pair[0] === variable) {
+            return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+        }
     }
-  }
+}
 
-  var searchTerm = getQueryVariable('query');
+// Search the site and search results
+function getSearchResults(searchTerm) {
+    if (searchTerm) {
+        document.getElementById('search-box').setAttribute("value", searchTerm);
 
-  if (searchTerm) {
-    document.getElementById('search-box').setAttribute("value", searchTerm);
+        // Initalize lunr with the fields it will be searching on. I've given title
+        // a boost of 10 to indicate matches on this field are more important.
+        var idx = lunr(function () {
+            this.field('id');
+            this.field('title', { boost: 10 });
+            this.field('author');
+            this.field('category');
+            this.field('content');
+        });
 
-    // Initalize lunr with the fields it will be searching on. I've given title
-    // a boost of 10 to indicate matches on this field are more important.
-    var idx = lunr(function () {
-      this.field('id');
-      this.field('title', { boost: 10 });
-      this.field('author');
-      this.field('category');
-      this.field('content');
+        for (var key in window.store) { // Add the data to lunr
+            idx.add({
+                'id': key,
+                'title': window.store[key].title,
+                'author': window.store[key].author,
+                'category': window.store[key].category,
+                'content': window.store[key].content
+            });
+
+        }
+
+        var results = idx.search(searchTerm); // Get lunr to perform a search
+
+        return results;
+    }
+}
+
+// Search the site and display search suggestions
+function getSearchSuggestions(searchTerm) {
+    if (searchTerm) {
+        // Perform a normal search, but convert results[] arr to only have result title
+        var results = getSearchResults(searchTerm);
+
+        var parsedResults = [];
+
+        if (results.length) { // Are there any results?
+            var appendString = '';
+
+            for (var i = 0; i < results.length; i++) {  // Iterate over the results
+                var item = store[results[i].ref];
+
+                if (item.title) {
+                    parsedResults.push(item.title);
+                }
+            }
+        }
+
+        return parsedResults;
+    }
+}
+
+function autocompleteSearchSuggestions() {
+    var inp = document.getElementById('search-box');
+
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) {
+            return false;
+        }
+        currentFocus = -1;
+
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+
+        // call to retrieve all search suggestions
+        var arr = getSearchSuggestions(val);
+
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+        }
     });
 
-    for (var key in window.store) { // Add the data to lunr
-      idx.add({
-        'id': key,
-        'title': window.store[key].title,
-        'author': window.store[key].author,
-        'category': window.store[key].category,
-        'content': window.store[key].content
-      });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
 
-      var results = idx.search(searchTerm); // Get lunr to perform a search
-      displaySearchResults(results, window.store); // We'll write this in the next section
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
     }
-  }
-})();
+
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
