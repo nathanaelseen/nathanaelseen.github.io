7be02e88498685fb4ec030b419a19b1cf9716e58
Nathanael Seen <e0345677@u.nus.edu>
2020-05-24 15:35:39 +0800

    Remove input highlight when click anywhere else in document

--- js/search.js
+++ js/search.js
@@ -220,5 +220,8 @@ function autocompleteSearchSuggestions() {
     /*execute a function when someone clicks in the document:*/
     document.addEventListener("click", function (e) {
         closeAllLists(e.target);
+
+        // Also remove any 
+        inp.style.borderColor = null;
     });
 }
