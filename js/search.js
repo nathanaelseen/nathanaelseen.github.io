0e62b88550a0af087dca34ed84e0ecbc62d586ee
Nathanael Seen <e0345677@u.nus.edu>
2020-05-24 18:02:17 +0800

    Remove hover classes on mobile devices

--- js/search.js
+++ js/search.js
@@ -216,13 +216,6 @@ function autocompleteSearchSuggestions() {
         }
     }
 
-    var isInpFocus = false;
-
-    inp.addEventListener("focus", function(e) {
-        alert("focus event");
-        isInpFocus = true;
-    });
-
     /*execute a function when someone clicks in the document:*/
     document.addEventListener("click", function (e) {
         closeAllLists(e.target);
@@ -233,17 +226,12 @@ function autocompleteSearchSuggestions() {
 
     /*execute a function when someone scrolls in the document:*/
     document.addEventListener("scroll", function (e) {
-        alert("scroll event");
         closeAllLists(e.target);
 
         // Also remove the borderColor on the searchfield 
         inp.style.borderColor = null;
         // inp.classList.remove("input:hover");
 
-        if (!isInpFocus) {
-            inp.blur();
-        } else {
-            isInpFocus = false;
-        }
+        inp.blur();
     });
 }
